a74cb3dfd82ed7269e05373c41dbbf46d152c556
Michael Prather <mprather1@gmail.com>
2017-09-11 00:23:10 +0000

    class

--- src/gpio.js
+++ src/gpio.js
@@ -4,37 +4,49 @@ import path from 'path'
 var sysPath = '/sys/class/gpio'
 const filePath = path.join(sysPath, 'gpio')
 
-function openPin (pinNumber, direction, callback) {
-  const file = path.join(filePath + pinNumber, 'direction')
-  fs.writeFile(file, direction, (callback || noOp))
-}
+export default class GPIO {
+  constructor (pins) {
+    this.pins = pins
+    this.exportPins(this.pins)
+  }
 
-function writePin (pinNumber, value, callback) {
-  const file = path.join(filePath + pinNumber, 'value')
-  console.log(file)
-  fs.writeFile(file, value, (callback || noOp))
-}
+  exportPins () {
+    for (let pin of this.pins) {
+      var file = ''
+      fs.stat(filePath + pin, (err, result) => {
+        if (err && err.code === 'ENOENT') {
+          console.log(`exporting pin ${pin}...`)
+          file = path.join(sysPath, 'export')
+          fs.writeFileSync(file, pin)
+        }
+      })
+    }
+  }
 
-function getValue (pinNumber, callback) {
-  const file = path.join(filePath + pinNumber, 'value')
-  fs.readFile(file, 'utf-8', callback)
-}
+  openPin (pinNumber, direction, callback) {
+    const file = path.join(filePath + pinNumber, 'direction')
+    fs.writeFile(file, direction, (callback || noOp))
+  }
 
-function getDirection (pinNumber, callback) {
-  const file = path.join(filePath + pinNumber, 'direction')
-  fs.readFile(file, 'utf-8', callback)
-}
+  writePin (pinNumber, value, callback) {
+    value = value ? '1' : '0'
+    const file = path.join(filePath + pinNumber, 'value')
+    fs.writeFile(file, value, (callback || noOp))
+  }
 
-function exportPin (pinNumber, exp, callback) {
-  fs.writeFile(sysPath + '/' + exp, pinNumber, (callback || noOp))
+  getValue (pinNumber, callback) {
+    const file = path.join(filePath + pinNumber, 'value')
+    fs.readFile(file, 'utf-8', callback)
+  }
+
+  getDirection (pinNumber, callback) {
+    const file = path.join(filePath + pinNumber, 'direction')
+    fs.readFile(file, 'utf-8', callback)
+  }
+
+  exportPin (pinNumber, exp) {
+    fs.writeFileSync(sysPath + '/' + exp, pinNumber)
+  }
 }
 
 function noOp () {}
-
-export default {
-  openPin,
-  writePin,
-  getValue,
-  getDirection,
-  exportPin
-}
